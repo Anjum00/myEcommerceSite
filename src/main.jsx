3af736311f332a9aa19b78ec428f0e70115dfeae
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2024-09-06 11:44:35 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import { store } from './Redux/Store.jsx';
-import {BrowserRouter} from "react-router-dom"
-import {ToastContainer} from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store} >
       <App />
-      <ToastContainer/>
+      <ToastContainer />
     </Provider>
-  </BrowserRouter>  
-)
+  </BrowserRouter>
+);
