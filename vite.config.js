738150da523410754e275b046ab84a1a95aa7dd8
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2024-09-16 16:28:22 +0530

    Updated ignore

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myEcommerceSite/', // Replace with your repository name
+  build: {
+    outDir: 'dist', 
+  },
   server: {
-    port: 3000, // Specify your desired port number here
-    base: '/myEcommerceSite/',
+    port: 3000, 
   },
 })
